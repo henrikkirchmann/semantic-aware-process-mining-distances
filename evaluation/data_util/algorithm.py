a4403b22606e2ec8ee90a3379fd1c5860895d0a2
Henrik Kirchmann <henrikkirchmann@gmail.com>
2024-07-01 20:33:15 +0200

    more evaluation

--- evaluation/data_util/algorithm.py
+++ evaluation/data_util/algorithm.py
@@ -1,9 +1,12 @@
-from typing import List, Tuple, Dict
 import itertools
-from copy import deepcopy
 import random
 from collections import defaultdict
-from distances.activity_distances.bose_2009_context_aware_trace_clustering.algorithm import get_substitution_and_insertion_scores
+from copy import deepcopy
+from typing import List
+
+from distances.activity_distances.bose_2009_context_aware_trace_clustering.algorithm import \
+    get_substitution_and_insertion_scores
+
 
 def get_log_control_flow_perspective(log):
     log_list = list()
@@ -12,10 +15,11 @@ def get_log_control_flow_perspective(log):
     i = 0
     for trace in log:
         for event in trace._list:
-            #log_list[i].append(event._dict.get('concept:name')+"-"+event._dict.get('lifecycle:transition'))
+            # log_list[i].append(event._dict.get('concept:name')+"-"+event._dict.get('lifecycle:transition'))
             log_list[i].append(event._dict.get('concept:name'))
         i += 1
     return log_list
+
 
 def get_alphabet(log: List[List[str]]) -> List[str]:
     unique_activities = set()
@@ -25,11 +29,13 @@ def get_alphabet(log: List[List[str]]) -> List[str]:
             unique_activities.add(activity)
     return list(unique_activities)
 
+
 def get_activities_to_replace(alphabet: List[str], different_activities_to_replace_count: int):
     return list(itertools.combinations(alphabet, different_activities_to_replace_count))
 
 
-def get_logs_with_replaced_activities_dict(activities_to_replace_in_each_run_list, log_control_flow_perspective, different_activities_to_replace_count, activities_to_replace_with_count):
+def get_logs_with_replaced_activities_dict(activities_to_replace_in_each_run_list, log_control_flow_perspective,
+                                           different_activities_to_replace_count, activities_to_replace_with_count):
     logs_with_replaced_activities_dict = dict()
     for activities_to_replace_tuple in activities_to_replace_in_each_run_list:
         log_with_replaced_activities = deepcopy(log_control_flow_perspective)
@@ -48,15 +54,41 @@ def get_logs_with_replaced_activities_dict(activities_to_replace_in_each_run_lis
         logs_with_replaced_activities_dict[activities_to_replace_tuple] = log_with_replaced_activities
     return logs_with_replaced_activities_dict
 
+
 def get_activity_distance_matrix_dict(activity_distance_functions, logs_with_replaced_activities_dict):
     activity_distance_matrix_dict = defaultdict(lambda: defaultdict())
     for activity_distance_function in activity_distance_functions:
-        activity_distance_matrix_dict[activity_distance_function][None] = None
-    for key in logs_with_replaced_activities_dict:
-        # Trace Distance with Levenshtein and Bose 2009
-        if "Bose 2009 Substitution Scores" in activity_distance_functions:
-            activity_distance_matrix = get_substitution_and_insertion_scores(logs_with_replaced_activities_dict[key],
-                                                                             get_alphabet(
-                                                                                 logs_with_replaced_activities_dict[
-                                                                                     key]), 3)
-            activity_distance_matrix_dict["Bose 2009 Substitution Scores"][key] = activity_distance_matrix
+        for key in logs_with_replaced_activities_dict:
+            # Trace Distance with Levenshtein and Bose 2009
+            if "Bose 2009 Substitution Scores" in activity_distance_functions:
+                activity_distance_matrix = get_substitution_and_insertion_scores(
+                    logs_with_replaced_activities_dict[key],
+                    get_alphabet(
+                        logs_with_replaced_activities_dict[
+                            key]), 3)
+                activity_distance_matrix_dict["Bose 2009 Substitution Scores"][key] = activity_distance_matrix
+    return dict(activity_distance_matrix_dict)
+
+
+def get_n_nearest_neighbors(n, replaced_activities, similarity_scores_of_activities, activities_to_replace_with_count):
+    neighbors = dict()
+    for replaced_activity in replaced_activities:
+        for i in range(activities_to_replace_with_count):
+            replaced_activity_i = replaced_activity + ':' + str(i)
+            # Collect all similarity scores for the current replaced_activity
+            distances = []
+            for (activity1, activity2), distance in similarity_scores_of_activities.items():
+                if replaced_activity_i == activity1:
+                    other_activity = activity2
+                    distances.append((other_activity, distance))
+
+            # Sort distances by the similarity score (distance)
+            distances.sort(key=lambda x: x[1])
+
+            # Get the top n nearest neighbors
+            nearest_neighbors = [activity for activity, _ in distances[:n]]
+
+            # Store the nearest neighbors in the dictionary
+            neighbors[replaced_activity_i] = nearest_neighbors
+
+    return neighbors
--- evaluation/evaluation_of_activity_distances/evaluation_activity_distance.py
+++ evaluation/evaluation_of_activity_distances/evaluation_activity_distance.py
@@ -1,41 +1,43 @@
-import random
 from pm4py.statistics.variants.log import get as variants_module
-#from pm4py.algo.evaluation.earth_mover_distance import algorithm as emd_evaluator
+# from pm4py.algo.evaluation.earth_mover_distance import algorithm as emd_evaluator
+from pm4py.statistics.variants.log import get as variants_module
 from pm4py.objects.log.importer.xes import importer as xes_importer
-from evaluation.data_util.algorithm import get_log_control_flow_perspective, get_alphabet, get_activities_to_replace, get_logs_with_replaced_activities_dict, get_activity_distance_matrix_dict
-from collections import defaultdict
-from copy import deepcopy
-
-from distances.activity_distances.bose_2009_context_aware_trace_clustering.algorithm import get_substitution_and_insertion_scores
 
 from definitions import ROOT_DIR
+from evaluation.data_util.algorithm import get_log_control_flow_perspective, get_alphabet, get_activities_to_replace, \
+    get_logs_with_replaced_activities_dict, get_activity_distance_matrix_dict, get_n_nearest_neighbors
 
 log = xes_importer.apply(ROOT_DIR + '/repairExample.xes')
 
-log_control_flow_perspective = get_log_control_flow_perspective(log)#transform log to a list of lists of activity labels
+log_control_flow_perspective = get_log_control_flow_perspective(
+    log)  # transform log to a list of lists of activity labels
 alphabet = get_alphabet(log_control_flow_perspective)
 
-
 different_activities_to_replace_count = 2
-activities_to_replace_with_count = 3
+activities_to_replace_with_count = 10
 
-#1: get the activities that we want to replace in each run
+# 1: get the activities that we want to replace in each run
 activities_to_replace_in_each_run_list = get_activities_to_replace(alphabet, different_activities_to_replace_count)
-#2: replace activities
-logs_with_replaced_activities_dict = get_logs_with_replaced_activities_dict(activities_to_replace_in_each_run_list, log_control_flow_perspective, different_activities_to_replace_count, activities_to_replace_with_count)
+# 2: replace activities
+logs_with_replaced_activities_dict = get_logs_with_replaced_activities_dict(activities_to_replace_in_each_run_list,
+                                                                            log_control_flow_perspective,
+                                                                            different_activities_to_replace_count,
+                                                                            activities_to_replace_with_count)
 
-#3: compute for all logs all activity distance matrices
+# 3: compute for all logs all activity distance matrices
 # activity_distance_functions we want to evaluate
 activity_distance_functions = []
 activity_distance_functions.append("Bose 2009 Substitution Scores")
 
-activity_distance_matrix_dict = get_activity_distance_matrix_dict(activity_distance_functions, logs_with_replaced_activities_dict)
+activity_distance_matrix_dict = get_activity_distance_matrix_dict(activity_distance_functions,
+                                                                  logs_with_replaced_activities_dict)
 
-#4: evaluation of all activity distance matrices
+# 4: evaluation of all activity distance matrices
+for replaced_activities in activity_distance_matrix_dict["Bose 2009 Substitution Scores"].keys():
+    nearest_neighbors = get_n_nearest_neighbors(activities_to_replace_with_count, replaced_activities,
+                                                activity_distance_matrix_dict["Bose 2009 Substitution Scores"][
+                                                    replaced_activities], activities_to_replace_with_count)
 
-
-activity_distance_matrix_dict = dict(activity_distance_matrix_dict)
 language = variants_module.get_language(log)
 
-print("a")
-#emd = emd_evaluator.apply(language, language)
+# emd = emd_evaluator.apply(language, language)
